473cabbcf9377d14ff4db8a3f56f763c912d045a
Aiana Abdyrakhmanova <73655002+luianqi@users.noreply.github.com>
2022-11-22 19:03:53 +0600

    initial

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Golang" // changeable
+	const conferenceTickets = 87  // not changeable
+	fmt.Println(conferenceName, conferenceTickets)
+}
